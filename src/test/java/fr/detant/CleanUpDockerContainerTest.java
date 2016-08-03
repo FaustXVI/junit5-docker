074c06eee61e5854bebd2d45433f4bafa3e93107
FaustXVI <xavier.detant@gmail.com>
2016-08-03 15:43:06 +0200

    Refactor tests

---
+++ src/test/java/fr/detant/CleanUpDockerContainerTest.java
@@ -0,0 +1,45 @@
+package fr.detant;
+
+import com.github.dockerjava.api.DockerClient;
+import com.github.dockerjava.core.DockerClientBuilder;
+import com.github.dockerjava.core.DockerClientConfig;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CleanUpDockerContainerTest {
+
+    private static int beforeTestContainerCount;
+
+    @BeforeAll
+    static void countExistingContainers() {
+        beforeTestContainerCount = countContainers();
+    }
+
+    @AfterAll
+    static void verifyNoContainerAdded() {
+        assertEquals(beforeTestContainerCount, countContainers(), "Containers should be remove after execution");
+    }
+
+    @Docker(image = "luisbebop/echo-server", ports = "8801:8800")
+    @Nested
+    public class FirstStartDockerContainerTest {
+
+        @Test
+        void aTestThatUseTheContainer() {
+        }
+    }
+
+    private static DockerClient dockerClient = DockerClientBuilder.getInstance(
+            DockerClientConfig.createDefaultConfigBuilder()
+                    .withApiVersion("1.22")
+                    .build())
+            .build();
+
+    private static int countContainers() {
+        return dockerClient.listContainersCmd().withShowAll(true).exec().size();
+    }
+}
