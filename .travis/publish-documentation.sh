affbf4a897828a58f4d3f1eabf5b9ce8de7e1df1
FaustXVI <xavier.detant@gmail.com>
2016-12-07 18:57:22 +0100

    Push over ssh

--- .travis/publish-documentation.sh
+++ .travis/publish-documentation.sh
@@ -14,6 +14,7 @@ function prepare_ssh {
 function configure_git {
     git config --global user.email "travis@travis-ci.org"
     git config --global user.name "travis-ci"
+    git remote set-url origin git@github.com:FaustXVI/junit5-docker.git
 }
 
 function get_version {
