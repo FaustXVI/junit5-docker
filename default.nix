ae375c4d89db1d4368c3650e51f460c4a5f739d9
FaustXVI <xavier.detant@gmail.com>
2017-04-10 19:58:18 +0200

    Update dependencies versions

--- default.nix
+++ default.nix
@@ -3,8 +3,8 @@ let
 in
 { pkgs ? import (_pkgs.fetchFromGitHub { owner = "NixOS";
                                          repo = "nixpkgs-channels";
-                                         rev = "759620505595d72879d1d8c74a59c0868cce8f71";
-                                         sha256 = "05x9szam0yqjdiz69px165krzsycsa419yq0zsv6s5lczmbl7cvn";
+                                         rev = "1849e695b00a54cda86cb75202240d949c10c7ce";
+                                         sha256 = "1fw9ryrz1qzbaxnjqqf91yxk1pb9hgci0z0pzw53f675almmv9q2";
                                        }) {}
 }:
 
